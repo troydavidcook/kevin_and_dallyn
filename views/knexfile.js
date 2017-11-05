9623f46289247ae9d85bedcd955d6500e78d5659
Troy David Cook <www.troydavidcook@gmail.com>
2017-11-05 13:24:11 -0700

    Added main script file and forms for singing up and logging in.

---
+++ views/knexfile.js
@@ -0,0 +1,10 @@
+// Update with your config settings.
+
+module.exports = {
+    
+      development: {
+        client: 'postgresql',
+        connection: 'postgres://localhost/kevin_dallyn_db'
+      }
+    };
+    
